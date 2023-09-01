5d98f7ee4ff79b9d638bf247062472882de4369c
Luiza Fagundes <143561107+luizafagundesx@users.noreply.github.com>
2023-09-01 19:32:56 -0300

    Add files via upload

---
+++ vince_beauty (2).sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26/08/2023 às 02:49
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `vince_beauty`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` int(11) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `telefone` int(11) DEFAULT NULL,
+  `origem` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `cpf`, `endereco`, `telefone`, `origem`) VALUES
+(1, 'Maira Silva', 123456789, 'Rua 5, casa 7', 988887777, 'Indicação'),
+(2, 'Ana Banana', 987654321, 'Rua A, Casa B', 955554444, 'Instagram'),
+(3, 'Leuriscleia Santos', 1011121314, 'Rua 5, Casa 12', 940028922, 'Facebook');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `preco` double(10,2) DEFAULT NULL,
+  `marca` varchar(25) DEFAULT NULL,
+  `validade` date DEFAULT NULL,
+  `estoque` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `marca`, `validade`, `estoque`) VALUES
+(1, 'Batom Matte ', 24.00, 'Ruby Rose', '2025-05-01', 200),
+(2, 'Máscara de Cílios - Volume', 32.00, 'Luisance', '2025-02-24', 90),
+(3, 'Paleta de Sombra', 48.00, 'Vivai', '2025-01-05', 150);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `id` int(11) NOT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `vendedor_id` int(11) DEFAULT NULL,
+  `produto_id` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vendas`
+--
+
+INSERT INTO `vendas` (`id`, `cliente_id`, `vendedor_id`, `produto_id`, `quantidade`) VALUES
+(2, 3, 1, 2, 10);
+
+--
+-- Acionadores `vendas`
+--
+DELIMITER $$
+CREATE TRIGGER `atualiza_estoque_venda` AFTER INSERT ON `vendas` FOR EACH ROW BEGIN
+	DECLARE produto_id int;
+    DECLARE quantidade_vendida int;
+
+    SET produto_id = new.produto_id;
+    SET quantidade_vendida = new.quantidade;
+
+	UPDATE produtos
+	SET estoque = estoque - quantidade_vendida
+    WHERE id = produto_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` int(11) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `telefone` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vendedores`
+--
+
+INSERT INTO `vendedores` (`id`, `nome`, `cpf`, `endereco`, `telefone`) VALUES
+(1, 'Caroline Ortiz', 1454599006, 'Rua Rodrigues da Costa 791', 999932767),
+(2, 'Jair Sousa', 1467540030, 'Rua Coronel Eduzinho, 367', 984082098),
+(3, 'Lorenzo Zanatta', 143321032, 'Rua dom Claudio José Gonçalves Ponce de Leão, 140', 993631330);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cliente_id` (`cliente_id`),
+  ADD KEY `fk_vendedor_id` (`vendedor_id`),
+  ADD KEY `fk_produto_id` (`produto_id`);
+
+--
+-- Índices de tabela `vendedores`
+--
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `fk_cliente_id` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
+  ADD CONSTRAINT `fk_produto_id` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`id`),
+  ADD CONSTRAINT `fk_vendedor_id` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedores` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
